0c55152848d22566415dcecbf9d34ee6a0cc2c71
chcamp <c_campos_cca@hotmail.com>
2023-10-05 13:00:39 -0500

    Agregar restController a main con metodo home()

--- src/main/java/com/proempresa/SpringBootBackendProApplication.java
+++ src/main/java/com/proempresa/SpringBootBackendProApplication.java
@@ -1,17 +1,32 @@
 package com.proempresa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.proempresa", "com.proempresa.config"})
 public class SpringBootBackendProApplication {
-
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hola Christian!! desde el SpringBootBackendProApplication "
+				+ " como RestController";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendProApplication.class, args);
 		
-		System.out.println("Hola este es el cambi de devops!!");
+		//System.out.println("Hola este es el cambi de devops!!");
+		
+		
 	}
+	
+	
 
 }
